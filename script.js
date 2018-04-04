a8542559c970657167666a7d43391e48f92f45a7
Beatrix <bealearnscode@gmail.com>
2018-04-04 01:45:26 +0000

    add jquery and filter functionality

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let elem = document.querySelector('.grid');
-let iso = new Isotope( elem, {
+// let elem = document.querySelector('.grid');
+// let iso = new Isotope( elem, {
+//   // options
+//   itemSelector: '.grid-item',
+//   layoutMode: 'fitRows'
+// });
+
+// init Isotope
+var $grid = $('.grid').isotope({
   // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
 });
