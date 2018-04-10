8f78553664f4b0859a5b6a91704af2e95d9c27e1
Beatrix <bealearnscode@gmail.com>
2018-04-10 16:24:00 +0000

    added checkbox functionality

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-
 // init Isotope
 var $grid = $('.grid').isotope({
-  // options
+  itemSelector: '.item'
 });
-// filter items on button click
-$('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
+
+
+$(function(){
+
+  var $container = $('.container'),
+      $checkboxes = $('.filter-checkbox-group input');
+  
+  $container.isotope({
+    itemSelector: '.grid-item'
+  });
+  
+  $checkboxes.change(function(){
+    var filters = [];
+    // get checked checkboxes values
+    $checkboxes.filter(':checked').each(function(){
+      filters.push( this.value );
+    });
+    // ['.undergraduate', '.minor'] -> '.undergraduate, .minor'
+    filters = filters.join(', ');
+    $container.isotope({ filter: filters });
+  });
+  
 });
+
+
+
+// // init Isotope
+// var $grid = $('.grid').isotope({
+//   // options
+// });
+// // filter items on button click
+// $('.filter-button-group').on( 'click', 'button', function() {
+//   var filterValue = $(this).attr('data-filter');
+//   $grid.isotope({ filter: filterValue });
+// });
