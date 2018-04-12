05159f00f69c364a79709b0475c11dd81c93af87
Beatrix <bealearnscode@gmail.com>
2018-04-12 14:44:35 +0000

    uncomment button rules. comment checkbox rules.

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-$(function(){
+// $(function(){
 
-  let $grid = $('.grid'),
-      $checkboxes = $('.filter-checkbox-group input');
+//   let $grid = $('.grid'),
+//       $checkboxes = $('.filter-checkbox-group input');
   
-  $grid.isotope({
-    itemSelector: '.grid-item'
-  });
+//   $grid.isotope({
+//     itemSelector: '.grid-item'
+//   });
   
-  $checkboxes.change(function(){
-    let filters = [];
-    // get value of checked boxes
-    $checkboxes.filter(':checked').each(function(){
-      filters.push( this.value );
-    });
-    // ['.undergraduate', '.minor'] -> '.undergraduate, .minor'
-    filters = filters.join(', ');
-    $grid.isotope({ filter: filters });
-  });
+//   $checkboxes.change(function(){
+//     let filters = [];
+//     // get value of checked boxes
+//     $checkboxes.filter(':checked').each(function(){
+//       filters.push( this.value );
+//     });
+//     // ['.undergraduate', '.minor'] -> '.undergraduate, .minor'
+//     filters = filters.join(', ');
+//     $grid.isotope({ filter: filters });
+//   });
   
+// });
+
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
 });
-
-
-
-// // init Isotope
-// var $grid = $('.grid').isotope({
-//   // options
-// });
-// // filter items on button click
-// $('.filter-button-group').on( 'click', 'button', function() {
-//   var filterValue = $(this).attr('data-filter');
-//   $grid.isotope({ filter: filterValue });
-// });
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
