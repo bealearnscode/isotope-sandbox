69777479f8d18eed4e621c86d3841e2e5dcadf7f
Beatrix <bealearnscode@gmail.com>
2018-04-19 18:28:02 +0000

    it is possible that the images are not loading until after isotope loads, creating a conflict in layout.  testing isotope's imageLoader function to fix the problem.

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ $(function(){
       $checkboxes = $('.filter-checkbox-group input');
   
   $grid.isotope({
-    itemSelector: '.grid-item'
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer'
+    }
   });
+  
+  // layout Isotope after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+  });  
   
   $checkboxes.change(function(){
     let filters = [];
