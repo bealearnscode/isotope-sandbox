9c71d0b935dd915d12bfddba5651f9ec31126387
Beatrix <bealearnscode@gmail.com>
2018-04-19 18:54:53 +0000

    layout out is perfect, but functionality is gone. need o run more tests on imageLoaded

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 $(function(){
+  
+  var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    columnWidth: '.grid-sizer'
+  }
+  let $checkboxes = document.getElementsByClassName('.filter-checkbox-group input');
+});
+// layout Isotope after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
+});  
 
-  let $grid = $('.grid'),
-      $checkboxes = $('.filter-checkbox-group input');
+  // let $grid = $('.grid'),
+  // let $checkboxes = document.getElementsByClassName('.filter-checkbox-group input');
   
-  $grid.isotope({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    masonry: {
-      columnWidth: '.grid-sizer'
-    }
-  });
   
-  // layout Isotope after each image loads
-  $grid.imagesLoaded().progress( function() {
-    $grid.isotope('layout');
-  });  
+  // $grid.imagesLoaded(
+  //   function(){
+  //     //isotope is initialized after images have loaded
+  //     $grid.isotope({
+  //       itemSelector: '.grid-item'
+  //   });
+  // })
+  
   
   $checkboxes.change(function(){
     let filters = [];
