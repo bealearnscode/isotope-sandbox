7e91522912f907187a7ad0211f643e96dd0419ba
Beatrix <bealearnscode@gmail.com>
2018-04-26 00:04:28 +0000

    masonry, the default layout, should work with setting fixed width to true, but the margins between grid items disappear

--- script.js
+++ script.js
@@ -1,49 +1,22 @@
-// use document.ready instead of window.load
-// window.load waits for all resources to be loaded first,
-// can be a long time
-$( function() {
-    // now doc is ready, make selection
-    // use another selector, not .isotope,
-    var $grid = $('.grid');
-    var $checkboxes = $('.filter-checkbox-group input');
-    // use imagesLoaded, instead of window.load
-    $grid.imagesLoaded( function() {
-        $grid.isotope({
-         itemSelector: '.grid-item',
-         percentPosition: true,
-        masonry: {
-          columnWidth: '.grid-sizer'
-        }
-    });
+// // use document.ready instead of window.load
+// // window.load waits for all resources to be loaded first,
+// // can be a long time
+// $( function() {
+//     // now doc is ready, make selection
+//     // use another selector, not .isotope,
+//     var $grid = $('.grid');
+//     var $checkboxes = $('.filter-checkbox-group input');
+//     // use imagesLoaded, instead of window.load
+//     $grid.imagesLoaded( function() {
+//         $grid.isotope({
+//          itemSelector: '.grid-item',
+//          masonry: {
+//             isFitWidth: true
+//         }
+//         }
+//     );
     
-    $checkboxes.change(function(){
-    let filters = [];
-    // get value of checked boxes
-    $checkboxes.filter(':checked').each(function(){
-      filters.push( this.value );
-    });
-    // ['.undergraduate', '.minor'] -> '.undergraduate, .minor'
-    filters = filters.join(', ');
-    $grid.isotope({ filter: filters });
-  });
-    });
-});
-
-
-
-
-
-
-// $(function(){
-
-//   let $grid = $('.grid'),
-//       $checkboxes = $('.filter-checkbox-group input');
-  
-//   $grid.isotope({
-//     itemSelector: '.grid-item'
-//   });
-  
-//   $checkboxes.change(function(){
+//     $checkboxes.change(function(){
 //     let filters = [];
 //     // get value of checked boxes
 //     $checkboxes.filter(':checked').each(function(){
@@ -53,11 +26,41 @@ $( function() {
 //     filters = filters.join(', ');
 //     $grid.isotope({ filter: filters });
 //   });
-
+//     });
 // });
 
 
+
+
+
+
+$(function(){
+
+  let $grid = $('.grid'),
+      $checkboxes = $('.filter-checkbox-group input');
+  
+  $grid.isotope({
+    itemSelector: '.grid-item',
+    masonry: {
+        isFitWidth: true
+    }
+  });
+  
+  $checkboxes.change(function(){
+    let filters = [];
+    // get value of checked boxes
+    $checkboxes.filter(':checked').each(function(){
+      filters.push( this.value );
+    });
+    // ['.undergraduate', '.minor'] -> '.undergraduate, .minor'
+    filters = filters.join(', ');
+    $grid.isotope({ filter: filters });
+  });
+
+});
+
 // $('.filter-button-group').on( 'click', 'button', function() {
-    //     var filterValue = $(this).attr('data-filter');
-    //     $container.isotope({ filter: filterValue });
-    // });
+//         var filterValue = $(this).attr('data-filter');
+//         $grid.isotope({ filter: filterValue });
+//     });
+
